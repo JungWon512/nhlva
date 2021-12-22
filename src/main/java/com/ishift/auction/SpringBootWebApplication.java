1b7544b4528997b6865482cb1f8360d52a7cd020
yuchansong410 <ycsong@ishift.co.kr>
2021-12-22 14:54:26 +0900

    21-12-22 : ycsong

--- src/main/java/com/ishift/auction/SpringBootWebApplication.java
+++ src/main/java/com/ishift/auction/SpringBootWebApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
