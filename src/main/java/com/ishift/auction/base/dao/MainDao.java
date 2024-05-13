e9d597f6fafc3a6df0fc5c1e9f972dde0b33a0a3
JungJungWon <86945288+JungWon512@users.noreply.github.com>
2024-05-13 11:40:39 +0900

    20240513 jjw -MainDao 기본값 lsCmeno 사번대신 사용자 아이디로 바인딩되게 수정

--- src/main/java/com/ishift/auction/base/dao/MainDao.java
+++ src/main/java/com/ishift/auction/base/dao/MainDao.java
@@ -125,7 +125,7 @@ public class MainDao {
     public void setDefaultValue(Map<String, Object> map) {
         map.put("regUsrid", map.getOrDefault("regUsrid", sessionUtill.getUserId()));
     	map.put("uptUsrid", map.getOrDefault("uptUsrid", sessionUtill.getUserId()));
-    	map.put("lsCmeno", sessionUtill.getEno());
+    	map.put("lsCmeno", sessionUtill.getUserId());
     	map.put("authRole", map.getOrDefault("authRole", sessionUtill.getRoleConfirm()));
     	map.put("naBzplc", map.getOrDefault("naBzplc", sessionUtill.getNaBzplc()));
     }
